0ca853c0fc6e6711806c92551602a7f460266fdf
Kate Grant <hello.kategrant@gmail.com>
2022-11-20 17:17:54 -0500

    added Item form

--- CustomNavigator.js
+++ CustomNavigator.js
@@ -4,7 +4,8 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import Closet from './Components/Closet';
 import AddItem from './Components/AddItem';
-import CameraRoll from './Components/CameraRoll';
+import CameraRollView from './Components/CameraRoll';
+import ItemForm from './Components/ItemForm';
 
 const Stack = createStackNavigator();
 
@@ -13,7 +14,8 @@ const AddItemsNavigator = () => {
     <Stack.Navigator>
       <Stack.Screen name="Closet" component={Closet} />
       <Stack.Screen name="AddItem" component={AddItem} />
-      <Stack.Screen name="CameraRoll" component={CameraRoll} />
+      <Stack.Screen name="CameraRoll" component={CameraRollView} />
+      <Stack.Screen name="ItemForm" component={ItemForm} />
     </Stack.Navigator>
   );
 };
