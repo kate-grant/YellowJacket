a38f724250356b5776bf1c947f6b78b3eed7c454
Kate Grant <hello.kategrant@gmail.com>
2022-11-19 21:32:21 -0500

    moved Closet and AddItem views into own components

---
+++ Components/AddItem.js
@@ -0,0 +1,11 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+
+export default function AddItem() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>AddItemPage</Text>
+    </View>
+  );
+}
---
+++ Components/Closet.js
@@ -0,0 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+
+export default function Closet({navigation}) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+      }}>
+      <Text>My Closet!</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('AddItem')}
+        style={styles.buttonStyle}>
+        <Text style={styles.btnText}>Add Item</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  btnText: {
+    color: '#F1FFBB',
+    textAlign: 'center',
+  },
+  buttonStyle: {
+    backgroundColor: '#D3BAF2',
+    height: 40,
+    width: 'fitContent',
+    padding: 10,
+    margin: 5,
+    alignItems: 'center',
+    borderRadius: 25,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+});
