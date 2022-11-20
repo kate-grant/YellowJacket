04dc0d13d9cd1340771f09c0c1171ae211844380
Kate Grant <hello.kategrant@gmail.com>
2022-11-20 01:27:22 -0500

    added photo buttons and navigation

--- Components/AddItem.js
+++ Components/AddItem.js
@@ -1,11 +1,47 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState} from 'react';
-import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import {useCameraRoll} from '@react-native-camera-roll/camera-roll';
+import {ScrollView} from 'react-native-gesture-handler';
 
-export default function AddItem() {
+export default function AddItem({navigation}) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F1FFBB',
+      }}>
       <Text>AddItemPage</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('AddItem')}
+        style={styles.buttonStyle}>
+        <Text style={styles.btnText}>Take Photo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('CameraRoll')}
+        style={styles.buttonStyle}>
+        <Text style={styles.btnText}>Upload from Gallery</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  btnText: {
+    color: '#F1FFBB',
+    textAlign: 'center',
+  },
+  buttonStyle: {
+    backgroundColor: '#D3BAF2',
+    height: 40,
+    width: 160,
+    padding: 10,
+    margin: 5,
+    alignItems: 'center',
+    borderRadius: 25,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+});
