51f1fa7ac3d443211d85d402ec3855666e7e73d8
Kate Grant <hello.kategrant@gmail.com>
2022-11-21 01:05:48 -0500

    added form elements and insert

--- Components/ItemForm.js
+++ Components/ItemForm.js
@@ -7,11 +7,39 @@ import {
   TouchableOpacity,
   StyleSheet,
   Image,
+  NativeModules,
 } from 'react-native';
 import {useCameraRoll} from '@react-native-camera-roll/camera-roll';
 import {ScrollView} from 'react-native-gesture-handler';
+import db, {errorCB} from '../Database/db';
 
 export default function ItemForm({navigation, route}) {
+  const [articleType, setArticleType] = useState('');
+  const [color, setColor] = useState('');
+
+  const setData = () => {
+    try {
+      db.transaction(tx => {
+        console.log('in set', articleType);
+        tx.executeSql(
+          'INSERT INTO Clothing (ArticleType, Colors, Img) VALUES (?, ?, ?)',
+          [articleType, color, route.params.image],
+          (tx, res) => {
+            console.log('success');
+            const length = res.rows.length;
+            if (length > 0) {
+              console.log('length', length);
+              res.rows.forEach(row => {
+                console.log(`inserted ${row.ID}`);
+              });
+            }
+          },
+        );
+      }, errorCB);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View
       style={{
@@ -28,7 +56,21 @@ export default function ItemForm({navigation, route}) {
         }}
         source={{uri: route.params.image}}
       />
-      <TextInput placeholder="Article (ie. Jacket)" style={styles.inputStyle} />
+      <TextInput
+        placeholder="Article (ie. Jacket)"
+        onChangeText={setArticleType}
+        style={styles.inputStyle}
+        value={articleType}
+      />
+      <TextInput
+        placeholder="Color (ie. Yellow)"
+        onChangeText={setColor}
+        style={styles.inputStyle}
+        value={color}
+      />
+      <TouchableOpacity style={styles.buttonStyle} onPress={() => setData()}>
+        <Text style={styles.btnText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -41,5 +83,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     borderRadius: 50,
     backgroundColor: '#E9E8E0',
+  },
+  btnText: {
+    color: '#F1FFBB',
+    textAlign: 'center',
+  },
+  buttonStyle: {
+    backgroundColor: '#D3BAF2',
+    height: 40,
+    width: 300,
+    padding: 10,
+    marginTop: 40,
+    alignItems: 'center',
+    borderRadius: 25,
+    display: 'flex',
+    justifyContent: 'center',
   },
 });
